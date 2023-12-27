b0bf71aee240bea670938d8d3f56f0d5ae3c66b2
Pratigya <pratigyadhakal4444@gmail.com>
2023-12-27 14:14:03 +0545

    author route added

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 require('dotenv').config();
 const adminRoutes = require('./src/routes/adminRoute');
+const authorRoutes = require('./src/routes/authorRoute');
 const bodyParser = require('body-parser');
 const apiRoutes = require('./src/routes/route')
 const app = express();
@@ -8,7 +9,7 @@ const port = process.env.PORT;
 app.use(bodyParser.json());
 app.use('/',apiRoutes);
 app.use('/api/admin', adminRoutes);
-
+app.use('/api/author',authorRoutes)
 
 app.listen(port, () => {
     console.log(`Server is running on port number ${port}`);
