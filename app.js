8b67008ed66d51f2cb4bb74df74a7c452c4d9575
Pratigya <pratigyadhakal4444@gmail.com>
2023-12-27 10:27:23 +0545

    [WIP]: Admin

--- app.js
+++ app.js
@@ -2,11 +2,13 @@ const express = require('express');
 require('dotenv').config();
 const adminRoutes = require('./src/routes/adminRoute');
 const bodyParser = require('body-parser');
-
+const apiRoutes = require('./src/routes/route')
 const app = express();
 const port = process.env.PORT; 
 app.use(bodyParser.json());
-app.use('/admin', adminRoutes);
+app.use('/',apiRoutes);
+app.use('/api/admin', adminRoutes);
+
 
 app.listen(port, () => {
     console.log(`Server is running on port number ${port}`);
