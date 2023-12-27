156a8d34a76db2dde3ecfed659c920ca1bfa20ad
Pratigya <pratigyadhakal4444@gmail.com>
2023-12-27 14:13:09 +0545

    add roles in jwt

--- src/utils/jwt.js
+++ src/utils/jwt.js
@@ -4,19 +4,8 @@ const SECRET = process.env.JWT_SECRET ;
 
 const jwt = require('jsonwebtoken');
 
-const generateToken = (userId,role) => {
-const token = jwt.sign({ userId, role }, SECRET, { expiresIn: '1h' });
-return token;
+const generateToken = (userId, role, expiresIn = '1h') => {
+    return jwt.sign({ userId, role }, SECRET, { expiresIn });
 };
 
-const verifyToken = (token) => {
-try {
-    const decoded = jwt.verify(token, SECRET);
-    return decoded;
-} catch (error) {
-    console.error('Token verification error:', error.message);
-    return null;
-}
-};
-
-module.exports = { generateToken, verifyToken };
+module.exports = { generateToken };
