f4dffe0ecfbfe5607d972ad29c9256b0b5f71de4
Pratigya <pratigyadhakal4444@gmail.com>
2023-12-28 13:03:10 +0545

    [Update]: Author Route

--- src/routes/authorRoute.js
+++ src/routes/authorRoute.js
@@ -1,7 +1,22 @@
 const express = require('express');
 const router = express.Router();
 const authorController = require('../controllers/authorController');
+const { authenticateAuthor } = require('../middleware/authorAuth.middlewere');
+const { validateLogin } = require('../utils/validate-login');
 
+router.post('/login', authorController.loginAuthor);
+
+router.use(authenticateAuthor);
+//create article
 router.post('/create-article', authorController.createArticle);
-
+//get author post only
+router.get('/get-post', authorController.getArticles);
+//get all author posts
+router.get('/view-all-posts', authorController.viewALLPosts);
+//update article
+router.put('/update-article/:id',authorController.updateArticle);
+//delete article
+router.delete('/delete-article/:id', authorController.deletePost);
+//update password
+router.put('/update-password',  authorController.updatePassword);
 module.exports = router;
