935c556583604741292138998dba1ed2d73c0361
Pratigya <pratigyadhakal4444@gmail.com>
2023-12-31 15:01:05 +0545

    [Updated] : Routes

--- src/routes/authorRoute.js
+++ src/routes/authorRoute.js
@@ -2,21 +2,21 @@ const express = require('express');
 const router = express.Router();
 const authorController = require('../controllers/authorController');
 const { authenticateAuthor } = require('../middleware/authorAuth.middlewere');
-const { validateLogin } = require('../utils/validate-login');
 
 router.post('/login', authorController.loginAuthor);
 
 router.use(authenticateAuthor);
-//create article
+// create article
 router.post('/create-article', authorController.createArticle);
-//get author post only
+// get author post only
 router.get('/get-post', authorController.getArticles);
-//get all author posts
+// get all author posts
 router.get('/view-all-posts', authorController.viewALLPosts);
-//update article
-router.put('/update-article/:id',authorController.updateArticle);
-//delete article
-router.delete('/delete-article/:id', authorController.deletePost);
-//update password
-router.put('/update-password',  authorController.updatePassword);
+// update article
+router.put('/update-article/:id', authorController.updateArticle);
+// delete article
+router.delete('/delete-article/:id', authorController.deleteArticle);
+// update password
+router.put('/update-password', authorController.updatePassword);
+
 module.exports = router;
