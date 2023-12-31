18dc7d18bddf955e6f09b8ba505ac42082bf485a
Pratigya <pratigyadhakal4444@gmail.com>
2023-12-31 15:01:44 +0545

    [Updated] : author middlewere

--- src/middleware/authorAuth.middlewere.js
+++ src/middleware/authorAuth.middlewere.js
@@ -1,28 +1,50 @@
+require('dotenv').config();
 const jwt = require('jsonwebtoken');
 const { PrismaClient } = require('@prisma/client');
 const SECRET = process.env.JWT_SECRET;
 
+const prisma = new PrismaClient();
 
-const authenticateAuthor = (req, res, next) => {
+const authenticateAuthor = async (req, res, next) => {
     try {
-        
-        const token = req.headers.authorization.split(' ')[1]; // Assuming the token is in the Authorization header
+        const authHeader = req.headers.authorization;
+
+        if (!authHeader || !authHeader.startsWith('Bearer ')) {
+        return res.status(401).json({ error: 'Unauthorized: Invalid token format' });
+        }
+
+        const token = authHeader.split(' ')[1];
 
         if (!token) {
-            return res.status(401).json({ error: 'Unauthorized: Missing token' });
+        return res.status(401).json({ error: 'Unauthorized: Missing token' });
         }
 
         const decoded = jwt.verify(token, SECRET);
-        req.user = decoded; 
+        // console.log('Decoded Token:', decoded);
 
-        const { role } = req.user;
-
-        // Check if the role is "AUTHOR"
-        if (role !== 'AUTHOR') {
-            return res.status(403).json({ error: 'Access Denied' });
+        if (!decoded || !decoded.userId || !decoded.role) {
+        return res.status(401).json({ error: 'Unauthorized: Invalid token payload' });
         }
 
-        next(); 
+        const user = await prisma.user.findUnique({
+        where: {
+            id: decoded.userId,
+        },
+        });
+
+        // console.log('Fetched User:', user);
+
+        if (!user || user.role !== 'AUTHOR') {
+        console.error('User not found or not an author:', user);
+        return res.status(403).json({ error: 'Access Denied' });
+        }
+
+        req.user = {
+        id: user.id,
+        role: user.role,
+        };
+
+        next();
     } catch (error) {
         console.error('Authentication error:', error);
         return res.status(401).json({ error: 'Unauthorized: Invalid token' });
